a357daae1353e244e8af5b1d09831f3cac94f29b
Nitin chugh <nitinchugh4u@gamil.com>
2022-10-17 01:06:24 +0530

    jira clone script.js first commit

---
+++ Browser/Jira-clone/script.js
@@ -0,0 +1,22 @@
+// return the first element that matches selector
+let addBtn = document.querySelector(".add-btn");
+console.log(addbtn);
+var isModalpresent =false;
+addBtn.addEventListener("click", function(){
+
+// case1 - if modal is not present then dispaly modal;
+
+ if( !isModalPresent){
+    // display modal
+ }
+
+ else if(isModalpresent){
+    // display none
+
+ }
+  
+ isModalPresent = !isModalPresent;
+
+})
+
+
---
+++ JS/intro.js
@@ -0,0 +1,34 @@
+// variable declearation
+var a;
+
+// variable initionalisation
+a = 10;
+
+// variable reinitialisation
+a = 20;
+// print
+console.log(a);
+
+// type of  data type
+console.log(typeof a);
+
+a = "hello";
+console.log(typeof a);
+ 
+a = true;
+console.log(typeof a);
+
+a = null ;
+console.log(typeof a);
+
+
+// data types in javascript
+
+// number
+// Bigint
+// Boolean
+// String
+// null
+// undefined
+// objects
+// symbol
